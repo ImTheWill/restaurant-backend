f2cac7ff61de750473a80a568a462ded4a35a0c4
ImTheWill <128171764+ImTheWill@users.noreply.github.com>
2024-03-12 21:50:41 -0700

    yess

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,7 +12,7 @@ export const signup = async (req, res, next) => {
     await userService.signup(username, email, password);
     console.log("SignUp Successful");
     
-    return res.status(201);
+    return res.status(200).json({message : 'Success'});
   } catch (err) {
     next(err);
   }
@@ -25,10 +25,10 @@ export const login = async (req, res, next) => {
     if (!email || !password) {
       throw new Error("Missing email or password!");
     }
+    console.log("before error 1")
     await userService.login(email, password);
-    console.log("Login Successful");
-
-    return res.status(200);
+    console.log("after error 1")
+    return res.status(200).json({ message: "Success" });
   } catch (err) {
     console.log(err);
     next(err);
--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ export const initServer = () => {
   app.use(morgan("dev"));
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors());
+  app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true,
+  }));
   app.use(router);
   
 
